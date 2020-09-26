dfc69170cde750b97b6354bf7f44ac0777ec4633
moises santos <moisesmember@gmail.com>
2020-09-26 13:26:00 -0400

    adição da sessão

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+    session: sessionData
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
