2c661feafb4c8a1e4a59634314c1b6dbd7e86f08
moises santos <moisesmember@gmail.com>
2020-09-26 13:16:42 -0400

    primeiro commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		client.sendMessage(message.from, 'pong');
+	}
+});
