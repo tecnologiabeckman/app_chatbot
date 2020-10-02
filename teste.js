abbabdfcb7d0398f5775ba6c3b52d8df4ba28b96
Moises Felipe dos Santos <moises.santos@ham2k3.local>
2020-10-02 12:32:01 -0400

    adição de emotions

--- teste.js
+++ teste.js
@@ -1,3 +1,44 @@
+/** Models mongoose */
+var sessaoMain = require('./model/sessao');
+require('dotenv').config();
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://10.51.19.55:27017/nps', {
+mongoose.connect(process.env.URL_DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+(async () =>{
+    const filter = { 
+        sessao : 'nps',
+        etapa : '07',
+        finalizada : 1,
+        telefone : '559294493350',
+        parametros: {nota:'MOISES SANTOS'}
+     };
+    //const update = { etapa: '03' };
+    //const update = { parametros: {nota:'MOISES SANTOS'} };
+    const update = { finalizada : 0 };
+    
+    mongoose.set('useFindAndModify', false);
+
+    let qtdeDoc = await sessaoMain.countDocuments(filter)
+    
+    console.log(`Qtde documentos: ${qtdeDoc}`); // 0
+    
+    let doc = await sessaoMain.findOneAndUpdate(filter, update, {
+      new: true,
+      upsert: true // Make this update into an upsert
+    });
+    
+    //doc.name; // Will Riker
+    //doc.age; // 29
+    
+    console.log(JSON.parse(JSON.stringify(doc)));
+})();
+
+
+
 var values = Array();
 
 values.push({
